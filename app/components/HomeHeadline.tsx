d71f0312a179e579976fd6bdec56475e8f9291de
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2026-02-12 14:06:37 +0530

    fix: headline & dryfruits images

--- app/components/HomeHeadline.tsx
+++ app/components/HomeHeadline.tsx
@@ -1,9 +1,21 @@
 export default function HomeHeadline() {
   return (
-    <div className="pt-[175px] pl-[115px] pb-[175px] w-full bg-[#F6F1E6] flex items-center justify-center pt-0 md:pt-10 overflow-x-hidden">
-        <h1 className="type-h1-italics-d italic font-extralight max-w-5xl pt-20 text-[#12385C]">
-            Lorem ipsum dolor sit amet, <span className="thin-weight">consectetur</span>  elit,   eiusmod tempor  ut <span className="thin-weight">labore</span> et dolore <span className="thin-weight">magnaui</span> aliqua
+    <section className="w-full bg-[#F6F1E6] overflow-x-hidden">
+      <div
+        className="
+          mx-auto w-full max-w-5xl
+          px-5 sm:px-8 md:px-10 lg:px-14
+          py-10 sm:py-14 md:py-16 lg:py-20 xl:py-24
+          flex items-center justify-center
+        "
+      >
+        <h1 className="type-h1-italics-d md:text-left text-[#12385C]">
+          Lorem ipsum dolor sit amet,{" "}
+          <span className="headline-muted">consectetur</span> elit, eiusmod tempor ut{" "}
+          <span className="headline-muted">labore</span> et dolore{" "}
+          <span className="headline-muted">magnaui</span> aliqua
         </h1>
-    </div>
+      </div>
+    </section>
   );
 }
