eccdfdb848d68b36a1ed5857e677daaf16fe57e2
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2026-02-13 19:58:06 +0530

    key_id error fixed

--- app/api/razorpay/order/route.ts
+++ app/api/razorpay/order/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
-    key_secret: process.env.RAZORPAY_KEY_SECRET!,
-});
-
 export async function POST(request: Request) {
+    const razorpay = new Razorpay({
+        key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+        key_secret: process.env.RAZORPAY_KEY_SECRET!,
+    });
+
     try {
         const { amount, currency } = await request.json();
 
